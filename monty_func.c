2fa8ee6b49fe9b6ac26d94237e724de458f65428
nureex <nureexamahmud@gmail.com>
2024-06-05 18:24:42 -0700

    monty math functions

---
+++ monty_func.c
@@ -0,0 +1,158 @@
+#include "monty.h"
+
+/**
+ * read_file - Read a bytecode file and execute commands
+ * @filename: Pathname to the file
+ * @stack: Pointer to the top of the stack
+ */
+void read_file(char *filename, stack_t **stack)
+{
+    char *line;
+    size_t i = 0;
+    int line_count = 1;
+    instruct_func s;
+    int check;
+    int read;
+
+    // Open the file for reading
+    var_global.file = fopen(filename, "r");
+
+    // Check if file opening was successful
+    if (var_global.file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    // Read each line of the file
+    while ((read = getline(&var_global.buffer, &i, var_global.file)) != -1)
+    {
+        line = parse_line(var_global.buffer, stack, line_count);
+
+        // Skip blank lines and comments
+        if (line == NULL || line[0] == '#')
+        {
+            line_count++;
+            continue;
+        }
+
+        // Get the function corresponding to the opcode
+        s = get_op_func(line);
+
+        // Check if the opcode is valid
+        if (s == NULL)
+        {
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_count, line);
+            exit(EXIT_FAILURE);
+        }
+
+        // Execute the function corresponding to the opcode
+        s(stack, line_count);
+        line_count++;
+    }
+
+    // Free dynamically allocated memory and close the file
+    free(var_global.buffer);
+    check = fclose(var_global.file);
+    if (check == -1)
+        exit(-1);
+}
+
+/**
+ * get_op_func - Check the opcode and return the corresponding function
+ * @str: The opcode
+ * Return: Returns a function pointer, or NULL on failure
+ */
+instruct_func get_op_func(char *str)
+{
+    int i;
+
+    instruction_t instruct[] = {
+        {"push", _push},
+        {"pall", _pall},
+        {"pint", _pint},
+        {"pop", _pop},
+        {"swap", _swap},
+        {"add", _add},
+        {"nop", _nop},
+        {"sub", _sub},
+        {"mul", _mul},
+        {"div", _div},
+        {"mod", _mod},
+        {"pchar", _pchar},
+        {"pstr", _pstr},
+        {"rotl", _rotl},
+        {"rotr", _rotr},
+        {NULL, NULL},
+    };
+
+    // Search for the opcode in the instruction array
+    i = 0;
+    while (instruct[i].f != NULL && strcmp(instruct[i].opcode, str) != 0)
+    {
+        i++;
+    }
+
+    return (instruct[i].f);
+}
+
+/**
+ * isnumber - Check if a string represents a number
+ * @str: String to be checked
+ * Return: Returns 1 if the string is a number, 0 otherwise
+ */
+int isnumber(char *str)
+{
+    unsigned int i;
+
+    if (str == NULL)
+        return (0);
+    i = 0;
+    while (str[i])
+    {
+        if (str[0] == '-')
+        {
+            i++;
+            continue;
+        }
+        if (!isdigit(str[i]))
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+/**
+ * parse_line - Parse a line for an opcode and arguments
+ * @line: The line to be parsed
+ * @stack: Pointer to the head of the stack
+ * @line_number: The current line number
+ * Return: Returns the opcode or NULL on failure
+ */
+char *parse_line(char *line, stack_t **stack, unsigned int line_number)
+{
+    char *op_code, *push, *arg;
+    (void)stack;
+
+    push = "push";
+    // Tokenize the line to extract the opcode
+    op_code = strtok(line, "\n ");
+    if (op_code == NULL)
+        return (NULL);
+
+    // If the opcode is "push", extract the argument and convert it to an integer
+    if (strcmp(op_code, push) == 0)
+    {
+        arg = strtok(NULL, "\n ");
+        if (isnumber(arg) == 1 && arg != NULL)
+        {
+            var_global.push_arg = atoi(arg);
+        }
+        else
+        {
+            fprintf(stderr, "L%d: usage: push integer\n", line_number);
+            exit(EXIT_FAILURE);
+        }
+    }
+    return (op_code);
+}
---
+++ monty_math.c
@@ -0,0 +1,132 @@
+#include "monty.h"
+
+/**
+ * _sub - Subtract the top element from the second top element of the stack
+ * @stack: Pointer to the stack (linked list)
+ * @line_number: Line number where the opcode occurs
+ */
+void _sub(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = *stack;
+    int sub = 0, i = 0;
+
+    // Check if the stack is empty
+    if (tmp == NULL)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    // Count the number of elements in the stack
+    while (tmp)
+    {
+        tmp = tmp->next;
+        i++;
+    }
+
+    // Check if there are fewer than two elements in the stack
+    if (stack == NULL || (*stack)->next == NULL || i <= 1)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    // Subtract the top element from the second top element and update the stack
+    sub = (*stack)->next->n - (*stack)->n;
+    _pop(stack, line_number);
+
+    (*stack)->n = sub;
+}
+
+/**
+ * _mul - Multiply the top two elements of the stack
+ * @stack: Pointer to the stack (linked list)
+ * @line_number: Line number where the opcode occurs
+ */
+void _mul(stack_t **stack, unsigned int line_number)
+{
+    int aux;
+
+    // Check if the stack has fewer than two elements
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+        free(var_global.buffer);
+        fclose(var_global.file);
+        free_dlistint(*stack);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        aux = (*stack)->n;
+        _pop(stack, line_number);
+        (*stack)->n *= aux;
+    }
+}
+
+/**
+ * _div - Divide the second top element by the top element of the stack
+ * @stack: Pointer to the stack (linked list)
+ * @line_number: Line number where the opcode occurs
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+    int div = 0;
+
+    // Check if the stack has fewer than two elements or if division by zero is attempted
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+        free(var_global.buffer);
+        fclose(var_global.file);
+        free_dlistint(*stack);
+        exit(EXIT_FAILURE);
+    }
+    else if ((*stack)->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", line_number);
+        free(var_global.buffer);
+        fclose(var_global.file);
+        free_dlistint(*stack);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        div = (*stack)->n;
+        _pop(stack, line_number);
+        (*stack)->n /= div;
+    }
+}
+
+/**
+ * _mod - Compute the remainder of the division of the second top element by the top element of the stack
+ * @stack: Pointer to the stack (linked list)
+ * @line_number: Line number where the opcode occurs
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+    int mod = 0;
+
+    // Check if the stack has fewer than two elements or if division by zero is attempted
+    if (*stack == NULL || (*stack)->next == NULL)
+    {
+        fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+        free(var_global.buffer);
+        fclose(var_global.file);
+        free_dlistint(*stack);
+        exit(EXIT_FAILURE);
+    }
+    else if ((*stack)->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", line_number);
+        free(var_global.buffer);
+        fclose(var_global.file);
+        free_dlistint(*stack);
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        mod = (*stack)->n;
+        _pop(stack, line_number);
+        (*stack)->n %= mod;
+    }
+}
